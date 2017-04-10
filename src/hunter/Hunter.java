206c3242e180053e2611873de42afc2ac32d2f48
LNorbi <lestar.norbert@gmail.com>
2017-04-10 18:48:40 +0200

    Improved main function and choosing screen.

--- src/hunter/Hunter.java
+++ src/hunter/Hunter.java
@@ -4,8 +4,30 @@ import model.GameStarter;
 
 public class Hunter {
 
+    /**
+     * Starts new game with the default value.
+     * If an integer argument is provided, it starts the game with the
+     * value of the argument.
+     * @param args - optional: (int) number of games, (int) game size
+     */
     public static void main(String[] args) {
+        int num_of_games = 8;
+        int game_size_difference = 16;
+        try {
+            if (args.length > 0) {
+                num_of_games = Integer.parseInt(args[0]);
+                game_size_difference = num_of_games;
+            }
+            if (args.length > 1) {
+                game_size_difference = Integer.parseInt(args[0]);
+            }
+        } catch (NumberFormatException e) {
+            System.err.println(e.getMessage());
+        }
+        new GameStarter(num_of_games, game_size_difference);
         new GameStarter(4);
+        new GameStarter(16);
+        new GameStarter(12);
     }
     
 }
--- src/model/GameStarter.java
+++ src/model/GameStarter.java
@@ -4,45 +4,114 @@ import java.awt.FlowLayout;
 import java.awt.event.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
- *
- * @author lestarn
+ *  A class which provides a starting interface to choose what game sizes
+ *  and mode to start with.
+ *  It also provides a function for closing the game for further use.
+ *  @author lestarn
  */
 public class GameStarter extends JFrame {
     
-    private final int NUM_OF_GAMES;
-    private final int GAME_SIZE_DIFFERENCE;
+    private final int DEFAULT_SIZE = 3;
+    
+    private int NUM_OF_GAMES;
+    private int GAME_SIZE_DIFFERENCE;
     
     public GameStarter() {
-        NUM_OF_GAMES = GAME_SIZE_DIFFERENCE = 3;
+        this(0, 0);
     }
     
     public GameStarter(final int n) {
-        NUM_OF_GAMES = GAME_SIZE_DIFFERENCE = n;
+        this(n, n);
+    }
+    
+    public GameStarter(final int n, final int m) {
+        if (2 < n && n < 16 && 1 < m && m < 10) {
+            setGameSize(n, n);
+        } else {
+            setDefaultSize();
+        }
         initialize();
+    }
+    
+    private void setGameSize(int n, int m) {
+        NUM_OF_GAMES = n;
+        GAME_SIZE_DIFFERENCE = m;
+    }
+    
+    private void setDefaultSize() {
+        setGameSize(DEFAULT_SIZE, DEFAULT_SIZE);
     }
     
     private void initialize() {
         setTitle("Vadászat");
-        setBounds(100, 100, 310, 100);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        getContentPane().setLayout( new FlowLayout(FlowLayout.CENTER, 5, 5) );
-        setVisible(true);
-        
-        for (int i = GAME_SIZE_DIFFERENCE
-                ; i <= NUM_OF_GAMES * GAME_SIZE_DIFFERENCE
-                ; i += GAME_SIZE_DIFFERENCE) {
-            createGameButton(i);
+        int height = 60 * (NUM_OF_GAMES / 4);
+        if (4 > NUM_OF_GAMES) {
+            height = 75;
         }
+        setBounds(100, 100, 400, height);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        getContentPane().setLayout( new FlowLayout(FlowLayout.CENTER, 5, 5) );
+        
+        for (int i = 1; i <= NUM_OF_GAMES; ++i) {
+            createGameButton(i * GAME_SIZE_DIFFERENCE);
+        }
+        
+        exit(this);
+        
+        setVisible(true);
     }
     
     private void createGameButton(final int size) {
         JButton button = new JButton(size + "x" + size);
         button.addActionListener((ActionEvent e) -> {
-            // new GameWindow() ?
-            dispose();
+            final int ret = gameCreator();
+            if (-1 != ret) {
+                dispose();
+            }
         });
         getContentPane().add(button);
+    }
+    
+    private int gameCreator() {
+        final int choice = JOptionPane.showOptionDialog( null
+                        , "Hogyan szeretne játszani?"
+                        , "Játék kezdése"
+                        , JOptionPane.YES_NO_OPTION
+                        , JOptionPane.QUESTION_MESSAGE
+                        , null
+                        , new Object[] {"Offline", "Online"}
+                        , "Offline" );
+        if (0 == choice) {
+            // TODO
+        } else if (1 == choice) {
+            
+        }
+        return choice;
+    }
+    
+    public static void exit(JFrame frame) {
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent ev) {
+                exit();
+            }
+        });
+    }
+    
+    private static void exit() {
+        final int choice = JOptionPane.showOptionDialog( null
+                        , "Biztosan ki szeretne lépni?"
+                        , "Kilépés megerősítése"
+                        , JOptionPane.YES_NO_OPTION
+                        , JOptionPane.QUESTION_MESSAGE
+                        , null
+                        , new Object[] {"Igen", "Nem"}
+                        , "Nem" );
+        if (0 == choice) {
+            System.exit(0);
+        }
     }
 }
